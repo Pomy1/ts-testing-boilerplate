d6ee5c5a741ee4ce722fae9f90486b91cb70a061
Miguel Echávarri <miguelepmrs@gmail.com>
2020-09-26 11:36:43 +0200

    Test refactor

--- test/fizz-buzz-kata.test.ts
+++ test/fizz-buzz-kata.test.ts
@@ -3,8 +3,16 @@ import FizzBuz from './../src/fizzBuzz'
 describe('FizzBuz', () => {
     const fizzBuzz = new FizzBuz()
 
-    it('should be Fizz if number is divisible of 3', () => {
+    it('should be Fizz if number contains 3', () => {
         expect(fizzBuzz.fizzBuzz(6)).toEqual(fizzBuzz.FIZZ);
+        expect(fizzBuzz.fizzBuzz(13)).toEqual(fizzBuzz.FIZZ);
+        expect(fizzBuzz.fizzBuzz(93)).toEqual(fizzBuzz.FIZZ);
+    })
+
+    it('should be Buzz if number is divisible of 5', () => {
+        expect(fizzBuzz.fizzBuzz(5)).toEqual(fizzBuzz.BUZZ);
+        expect(fizzBuzz.fizzBuzz(10)).toEqual(fizzBuzz.BUZZ);
+        expect(fizzBuzz.fizzBuzz(85)).toEqual(fizzBuzz.BUZZ);
     })
 
     it('should be 1 if is one', () => {
@@ -13,16 +21,6 @@ describe('FizzBuz', () => {
 
     it('should be 1 if is one', () => {
         expect(fizzBuzz.fizzBuzz(7)).toEqual("7");
-    })
-
-    it('should be Fizz if number contains 3', () => {
-        expect(fizzBuzz.fizzBuzz(13)).toEqual(fizzBuzz.FIZZ);
-        expect(fizzBuzz.fizzBuzz(93)).toEqual(fizzBuzz.FIZZ);
-    })
-
-    it('should be Buzz if number is divisible of 5', () => {
-        expect(fizzBuzz.fizzBuzz(5)).toEqual(fizzBuzz.BUZZ);
-        expect(fizzBuzz.fizzBuzz(85)).toEqual(fizzBuzz.BUZZ);
     })
 
     it('should be FizzBuzz if number is divisible of 3 and 5', () => {
